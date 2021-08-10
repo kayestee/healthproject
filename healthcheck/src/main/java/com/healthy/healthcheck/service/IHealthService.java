1eaecd2fb7e28671555e7f6c11a8590aa9d51908
Kalyan <rivitu@Ks-Laptop.local>
2021-08-09 23:05:52 -0400

    removing

--- healthcheck/src/main/java/com/healthy/healthcheck/service/IHealthService.java
+++
@@ -1,9 +0,0 @@
-package com.healthy.healthcheck.service;
-
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
-public interface IHealthService {
-
-    ObjectNode healthCheckService(String format) throws Exception;
-
-}
